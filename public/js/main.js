f5f66cac00d4e8e511edb94a628b10b663403f3d
saifullah mohammed <saifmohammed1597@gmail.com>
2022-02-13 18:38:42 +0300

    info card updated

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,11 @@ const closebtn = document.querySelector('.closebtn');
 const toggleMenu =document.querySelector('.toggleMenu');
 const menu = document.querySelector('.menu');
 
+//.info.html
+const previous_icon = document.getElementById('previous_icon')
+const next_icon = document.getElementById('next_icon');
+const infocard = document.querySelector('.infocard');
+
 toggleMenu.addEventListener('click',() => {
     menu.classList.add('show')
 });
@@ -9,3 +14,13 @@ toggleMenu.addEventListener('click',() => {
 closebtn.addEventListener('click',() => {
     menu.classList.remove('show')
 });
+
+next_icon.addEventListener('click', () => {
+    infocard.scrollLeft += 240;
+});
+
+
+previous_icon.addEventListener('click', () => {
+    infocard.scrollLeft -= 240;
+});
+
